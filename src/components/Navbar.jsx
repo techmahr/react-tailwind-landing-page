9896f0348e6150141d9157bcc9c43c90b124943c
Mahroof <mahroof@Mac.lan>
2026-03-13 00:05:58 +0530

    other sections added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar({ scrolled }) {
                         <div>
                             <img
                                 src="/logo.png"
-                                alt="CodeFlow"
+                                alt="MahrCode"
                                 className="w-6 h-6 sm:w-8 sm:h-8"
                             />
                         </div>
