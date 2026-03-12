2de58b8fc91f954841dae7667904649eab5d17a5
Mahroof <mahroof@Mac.lan>
2026-03-12 16:37:18 +0530

    navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Menu } from 'lucide-react';
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div>
+            <nav className='fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm border-b'>
+                <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                    <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
+                        <div className='flex items-center space-x-1 group cursor-pointer'>
+                            <div>
+                                <img src="./logo.png" alt="logo" className='w-6 h-6 sm:w-8 sm:h-8' />
+                            </div>
+                            <span className='text-lg sm:text-xl md:text-2xl font-medium'>
+                                <span className='text-white'>Mahr</span>
+                                <span className='text-blue-500'>Code</span>
+                            </span>
+                        </div>
+
+                        {/* Nav links    */}
+
+                        <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+                            <a href="#features" className='text-gray-300 hover:text-white text-sm lg:text-base'>Features</a>
+                            <a href="#pricing" className='text-gray-300 hover:text-white text-sm lg:text-base'>Pricing</a>
+                            <a href="#testimonials" className='text-gray-300 hover:text-white text-sm lg:text-base'>Testimonials</a>
+                        </div>
+                        <button className="md:hidden p-2 text-gray-300 hover:text-white">
+                            <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
+                        </button>
+
+                    </div>
+                </div>
+            </nav >
+
+        </div >
     )
 }
 
